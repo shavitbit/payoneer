6b97e16c4ec39f2390fbf4094c8b3eea494707e4
devopsmaxit <git config --global user.email devopsmaxit@gmail.com>
2021-01-25 14:23:26 +0200

    add async to send method

--- counter-service/Controllers/thecounter.cs
+++ counter-service/Controllers/thecounter.cs
@@ -3,6 +3,7 @@ using RabbitMQ.Client;
 using System;
 using System.Globalization;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace counter_service.Controllers
 {
@@ -27,32 +28,40 @@ namespace counter_service.Controllers
             }
         }
  
-        private void send(string msg)
+        private async void Send(string msg)
         {
-            Uri uri = new Uri(connectionString);
-            var factory = new ConnectionFactory() { Uri = uri };
-            using (var connection = factory.CreateConnection())
-            using (var channel = connection.CreateModel())
+            await Task.Run(() =>
             {
-                channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
-                string message = msg;
-                var body = Encoding.UTF8.GetBytes(message);
-                
-                channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
-            }
+                Uri uri = new Uri(connectionString);
+                var factory = new ConnectionFactory() { Uri = uri };
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    string message = msg;
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+                }
+            });
         }
-      
+        /// <summary>
+        /// output the queue count 
+        /// </summary>
         [HttpGet]
         public uint Get()
         {
            return GetMessageCount();
         }
-
+        /// <summary>
+        /// send the datetime to the queue in async function and return string that indicate that the Post
+        /// request registered successfully.
+        /// </summary>
         [HttpPost]
         public string Post () 
         {
             DateTime dateTime = DateTime.Now;
-            send(dateTime.ToString("MM/dd/yyyy HH:mm:ss.fff", CultureInfo.InvariantCulture));
+            Send(dateTime.ToString("MM/dd/yyyy HH:mm:ss.fff", CultureInfo.InvariantCulture));
             return "a post request registered successfully ";
         }
     }
